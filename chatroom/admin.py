6786b1f0977377e70d58a3c6c125ca923a3033fb
clive <clivekakeeto48@gmail.com>
2023-09-05 00:18:56 +0300

    uncoment

--- chatroom/admin.py
+++ chatroom/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-# from django.contrib.auth.models import User
 from .models import User, Conversation,GroupChat, GroupMessage, AssistantMessage, Message, UserProfile
 
-# admin.site.register(User)
+
 admin.site.register(Conversation)
 admin.site.register(GroupChat)
 admin.site.register(GroupMessage)
