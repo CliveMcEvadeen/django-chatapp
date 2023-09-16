76204cbe993b44d45918cd20a4453295cb7d192c
clive <clivekakeeto48@gmail.com>
2023-09-16 18:28:10 +0300

    home modified

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "chatroom/static"]
 
 # Default primary key field type
